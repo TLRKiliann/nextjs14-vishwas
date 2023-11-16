e586be0090d0fbf7256a0ae13c1f2c05bb3dc157
TLRKiliann <philogenie@protonmail.com>
2023-11-16 21:47:54 +0100

    some comment added for params

--- my-app/app/products/[productId]/page.tsx
+++ my-app/app/products/[productId]/page.tsx
@@ -4,9 +4,11 @@ export default function DetailsProduct({params}: {params: {productId: string}}) 
     if (parseInt(params.productId) > 100) {
         notFound();
     }
+    console.log(params, "params")
     return (
         <div className='h-screen'>
             <p className="m-4">Details by product id: {params.productId}</p>
         </div>
     )
 }
+// params take the value of product.id
--- my-app/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ my-app/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,15 +1,15 @@
 import { notFound } from 'next/navigation';
 
-export default function ReviewById({params}: {params: {
-    productId: string, 
-    reviewId: string }
-    }) {
-        if (parseInt(params.reviewId) > 100) {
-            notFound();
-        }
+export default function ReviewById({ params }: {params: { productId: string, reviewId: string }}) {
+    if (parseInt(params.reviewId) > 100) {
+        notFound();
+    }
+    console.log(params.reviewId, "reviewid")
     return (
         <div className='h-sceen'>
             <p>Product id: {params.productId} and review id: {params.reviewId}</p>
         </div>
     )
 }
+
+// params take the string value of productId & reviewId
