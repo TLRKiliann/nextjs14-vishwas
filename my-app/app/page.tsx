977e98f513bb3e6c6fd710e3bc3750f7dcb27998
TLRKiliann <philogenie@protonmail.com>
2023-11-21 12:01:47 +0100

    Link replaced by useRouter

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 
 export default function Home() {
+  
+  const router = useRouter();
+
   return (
     <main className="w-full h-screen">
       <h1 className='text-2xl p-4'>Main Page</h1>
@@ -14,11 +20,12 @@ export default function Home() {
 
           <hr className='border-t border-slate-600' />
 
-          <Link href="/docs/concept/feature" 
-            className='text-blue-400 hover:bg-slate-700 py-2 pl-6 hover:text-blue-300'
+          <button onClick={() => router.push("/docs/concept/feature")}
+            className='flex justify-start text-blue-400 
+              hover:bg-slate-700 py-2 pl-6 hover:text-blue-300'
           >
             Docs
-          </Link>
+          </button>
 
       </div>
     </main>
