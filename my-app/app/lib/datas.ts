75e254df90c08357be6e32efd7ebb81f893582a1
TLRKiliann <philogenie@protonmail.com>
2023-11-25 23:40:06 +0100

    list of invoices expanded

--- my-app/app/lib/datas.ts
+++ my-app/app/lib/datas.ts
@@ -42,7 +42,7 @@ const customers: CustomersProps[] = [
         email: "james@mail.com",
         date: "2023-07-31",
         amount: 22,
-        status: "member"
+        status: "pending"
     },
     { 
         id: "2",
@@ -50,7 +50,7 @@ const customers: CustomersProps[] = [
         email: "noelia@mail.com",
         date: "2023-04-06",
         amount: 44,
-        status: "member"
+        status: "paid"
     },
     { 
         id: "3",
@@ -58,8 +58,64 @@ const customers: CustomersProps[] = [
         email: "doug@mail.com",
         date: "2023-11-09",
         amount: 33,
-        status: "member"
+        status: "pending"
     },
+    { 
+        id: "4",
+        name: "Katia",
+        email: "katia@mail.com",
+        date: "2023-02-19",
+        amount: 32,
+        status: "paid"
+    },
+    { 
+        id: "5",
+        name: "Nolwen",
+        email: "molwen@mail.com",
+        date: "2023-01-02",
+        amount: 44,
+        status: "pending"
+    },
+    { 
+        id: "6",
+        name: "Devola",
+        email: "devola@mail.com",
+        date: "2022-01-20",
+        amount: 28,
+        status: "paid"
+    },
+    { 
+        id: "7",
+        name: "Nancy",
+        email: "nancy@mail.com",
+        date: "2023-11-09",
+        amount: 38,
+        status: "pending"
+    },
+    { 
+        id: "8",
+        name: "Naëlle",
+        email: "naëlle@mail.com",
+        date: "2023-09-21",
+        amount: 29,
+        status: "paid"
+    },
+    { 
+        id: "9",
+        name: "Figo",
+        email: "figo@mail.com",
+        date: "2023-11-09",
+        amount: 21,
+        status: "pending"
+    },
+    { 
+        id: "10",
+        name: "Gill",
+        email: "gill@mail.com",
+        date: "2023-11-09",
+        amount: 44,
+        status: "pending"
+    }
 ];
 
 const ITEMS_PER_PAGE = 6;
@@ -67,16 +123,18 @@ export function fetchInvoicesPages(query: string) {
     try {
 
       const count = customers;
-      console.log(count, "count");
+      //console.log(count, "count");
   
-      const totalPages = Math.ceil(Number(count) / ITEMS_PER_PAGE); // 6 items/page
-      return totalPages;
+      const totalPages = 10 / ITEMS_PER_PAGE; // 6 items/page
+      const roundPage = Math.ceil(totalPages);
+      return roundPage;
     } catch (error) {
       console.error('Database Error:', error);
       throw new Error('Failed to fetch total number of invoices.');
     }
 }
 
+// trbl with table render for pagination (without query)
 export function fetchFilteredInvoices(query: string, currentPage: number) {
     //const offset = (currentPage - 1) * ITEMS_PER_PAGE;
     try {
