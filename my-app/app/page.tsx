84e952dfa64ce492030e2e5f36df269f4fade8fa
TLRKiliann <philogenie@protonmail.com>
2023-11-27 18:13:56 +0100

    design improved

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
-import forestImg from "@/public/img_bg/red_forest.jpg"
+import { BsCpu } from "react-icons/bs";
+import boxCpu from "@/public/img_cpu/box_cpu.jpg";
+import cpuI9 from "@/public/img_cpu/cpu_i9.jpg";
+import cpuI7 from "@/public/img_cpu/cpu_i7.jpg";
 
 export default function Home() {
   
+  const pathname = usePathname();
+  /*
+  console.log(pathname, "pathname");
+  */
+
   const router = useRouter();
 
   const handleClick = () => {
@@ -14,22 +23,22 @@ export default function Home() {
   }
 
   return (
-    <div className='relative text-blue-300 flex bg-black z-0'>
+    <div className='relative flex text-blue-300 bg-black z-0'>
       
-      <div className='fixed flex align-center justify-center w-full h-auto -z-10'>
+      <div className='fixed flex align-center justify-center w-full h-full m-auto -z-10'>
         <Image
-          src={forestImg}
-          width={1920}
-          height={1080}
+          src={boxCpu}
+          width={1192}
+          height={671}
           alt="cpu img" 
-          className='flex w-auto h-auto m-auto object-cover'
+          className='flex w-full h-auto m-auto object-cover'
         />
       </div>
 
       <main className="flex flex-col w-full min-h-screen">
   
         <div className='absolute mt-0 flex flex-col align-center justify-center w-full h-40 bg-cyan-50 
-          border border-b-blue-500 shadow-xl z-10'>
+          border border-b-blue-200 shadow-xl z-10'>
           
           <div className='flex flex-col h-40'>
             <p className='text-xl text-blue-800 font-bold m-auto mb-0'>The</p>
@@ -44,30 +53,27 @@ export default function Home() {
           </div>
 
           <div className='flex justify-around w-full text-lg text-blue-800 m-auto mb-1'>
-            <Link href="" className='hover:text-cyan-600'>Home</Link>
-            <Link href="" className='hover:text-cyan-600'>Products</Link>
-            <Link href="" className='hover:text-cyan-600'>About</Link>
-            <Link href="" className='hover:text-cyan-600'>Contact</Link>
-            <Link href="" className='hover:text-cyan-600'>Login</Link>
+            <Link href="/" className={`pathname === '/' ? text-cyan-600 : hover:text-cyan-600`}>Home</Link>
+            <Link href="/products" className='hover:text-cyan-600'>Products</Link>
+            <Link href="/about" className='hover:text-cyan-600'>About</Link>
+            <Link href="/contact" className='hover:text-cyan-600'>Contact</Link>
+            <Link href="/login" className='hover:text-cyan-600'>Login</Link>
           </div>
 
         </div>
 
-
-
-        <div className='relative flex align-center justify-center h-12 bg-cyan-100 
-          shadow-xl mt-40 z-0'>
-          <h2 className='text-2xl text-blue-800 m-auto'>Some text</h2>
+        <div className='relative flex align-center justify-center h-12 bg-cyan-100 shadow-xl mt-40 z-0'>
+          <h2 className='text-2xl text-blue-800 m-auto'>Welcome to our shop !</h2>
         </div>
         
         <div className='h-100 bg-cyan-100'>
-          <div className='flex w-2/5 max-w-lg max-h-xs bg-slate-900/20 border border-red-600/50 m-auto my-10 p-12'>
+          <div className='flex w-3/5 min-h-xs bg-slate-900/20 m-auto my-10 p-12 rounded-lg'>
             <Image
-              src={forestImg}
-              width={1920}
-              height={1080}
+              src={boxCpu}
+              width={1192}
+              height={671}
               alt="forest img"
-              className='flex w-auto h-auto m-auto border border-red-600/40 object-cover'
+              className='flex w-auto h-auto m-auto object-cover rounded-lg'
             />
           </div>
         </div>
@@ -120,11 +126,14 @@ export default function Home() {
           </aside>
 
           <div className='w-full bg-slate-900 border-b border-blue-400/20'>
+            
             <h2 className='text-3xl font-extrabold
               text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 from-10% 
-              via-sky-500 via-50% to-emerald-500 to-90% my-4 px-10'>
+              via-sky-500 via-50% to-emerald-500 to-90% my-4 px-10'
+            >
               First Title
             </h2>
+
             <p className='text-justify px-10'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
@@ -133,6 +142,7 @@ export default function Home() {
               fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
               culpa qui officia deserunt mollit anim id est laborum.
             </p>
+
           </div>
 
         </div>
@@ -140,65 +150,100 @@ export default function Home() {
         <div className='w-full h-20 bg-slate-900'>
         </div>
 
-        <div className='flex align-center justify-around w-full m-auto'>
 
-          <div className='w-[60px] bg-slate-900'>
+
+
+
+
+
+
+
+
+
+        <div className='flex w-full'>
+
+          <div className='w-[60px] h-auto bg-slate-900'>
           </div>
 
-          <div className='flex w-2/5 max-w-lg max-h-xs bg-slate-900/20 border border-red-600/50 m-auto my-10 p-12
-            hover:transition hover:-skew-y-6'>
-            <Image
-              src={forestImg}
-              width={1920}
-              height={1080}
-              alt="forest img"
-              className='flex w-auto h-auto m-auto border border-red-600/40 object-cover 
-                transition duration-0 hover:duration-300 hover:ease-in-out hover:skew-y-12'
-            />
+          <div className='flex align-center justify-around w-full m-auto shadow-in'>
+
+
+            <div className='flex w-2/5 max-w-lg max-h-xs bg-slate-900/20 shadow-out
+              m-auto my-10 p-12 hover:transition hover:-skew-y-6 rounded-lg'
+            >
+              <Image
+                src={cpuI9}
+                width={2157}
+                height={1440}
+                alt="forest img"
+                className='flex w-auto h-auto m-auto border border-slate-600/40 object-cover 
+                  transition duration-0 hover:duration-300 hover:ease-in-out hover:skew-y-12 rounded-lg'
+              />
+            </div>
+
+            <div className='w-6/12 max-w-xl flex flex-col text-blue-300 m-auto
+              transition ease-in-out rounded-lg
+              hover:scale-105 hover:text-blue-200 hover:shadow-2xl'>
+
+              <span className='h-10 border-none
+                bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% 
+                rounded-tl-lg rounded-tr-lg'>
+              </span >
+
+              <section className='w-full flex'>
+
+                <article className='h-auto bg-slate-900'>
+
+                  <h2 className='flex align-center text-2xl font-bold text-transparent bg-clip-text 
+                    bg-gradient-to-br from-indigo-500 from-10% 
+                  via-sky-500 via-50% to-emerald-500 to-90% py-4 pl-10'
+                  >
+                    CPU i9<BsCpu className="text-blue-400 mt-1 ml-4" />
+                  </h2>
+
+                  <p className='text-justify px-10 pb-5'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                    culpa qui officia deserunt mollit anim id est laborum.
+                  </p>
+
+                </article>
+
+              </section>
+
+              <span className='h-10 border-none
+                bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% 
+                rounded-bl-lg rounded-br-lg'>
+              </span >
+
+            </div>
+
           </div>
 
-          <div className='w-6/12 max-w-xl flex flex-col text-blue-300 m-auto my-10
-            transition ease-in-out 
-            hover:scale-105 hover:text-blue-200 hover:shadow-2xl'>
-
-            <span className='h-10 border-none
-              bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% 
-              rounded-tl-lg rounded-tr-lg'>
-            </span >
-
-            <section className='w-full flex'>
-
-              <article className='h-auto bg-slate-900'>
-                <h2 className='text-2xl font-bold py-4 pl-10'>Title 1</h2>
-                <p className='text-justify px-10 pb-5'>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                  Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-                  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                  culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-              </article>
-
-            </section>
-
-            <span className='h-10 border-none
-              bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% 
-              rounded-bl-lg rounded-br-lg'>
-            </span >
-
-          </div>
-
-          <div className='w-[60px] bg-slate-900'>
+          <div className='w-[60px] h-auto bg-slate-900'>
           </div>
 
         </div>
 
 
+
+
+
+
+
+
+
+
+
         <div className='w-full bg-slate-900 p-3'>
+
           <h2 className='w-[800px] text-2xl font-bold opacity-90 mx-auto mt-3 px-4 pb-4'>
             Title intermediate
           </h2>
+
           <p className='w-[800px] text-justify mx-auto px-4 mb-3'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
@@ -207,6 +252,7 @@ export default function Home() {
             fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
+
         </div>
 
 
@@ -215,8 +261,8 @@ export default function Home() {
           <div className='w-[60px] bg-slate-900'>
           </div>
 
-          <div className='w-6/12 max-w-xl flex flex-col text-blue-300 m-auto my-10
-            transition ease-in-out
+          <div className='w-6/12 max-w-xl flex flex-col text-blue-300 m-auto
+            transition ease-in-out rounded-lg
             hover:scale-105 hover:text-blue-200 hover:shadow-2xl'>
 
             <span className='h-10 border-none
@@ -227,7 +273,14 @@ export default function Home() {
             <section className='w-full flex'>
 
               <article className='h-auto bg-slate-900'>
-                <h2 className='text-2xl font-bold py-4 pl-10'>Title 2</h2>
+
+                <h2 className='flex align-center text-2xl font-bold text-transparent bg-clip-text 
+                  bg-gradient-to-br from-indigo-500 from-10% 
+                via-sky-500 via-50% to-emerald-500 to-90% py-4 pl-10'
+                >
+                  CPU i7<BsCpu className="text-blue-400 mt-1 ml-4" />
+                </h2>
+
                 <p className='text-justify px-10 pb-5'>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
                   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
@@ -236,6 +289,7 @@ export default function Home() {
                   fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
                   culpa qui officia deserunt mollit anim id est laborum.
                 </p>
+
               </article>
 
             </section>
@@ -247,13 +301,15 @@ export default function Home() {
 
           </div>
 
-          <div className='flex w-2/5 max-w-lg max-h-xs bg-slate-900/20 border border-red-600/50 m-auto my-10 p-12'>
+          <div className='flex w-2/5 max-w-lg max-h-xs bg-slate-900/20 border border-slate-400/50 
+            m-auto my-10 p-12 rounded-lg'
+          >
             <Image
-              src={forestImg}
-              width={1920}
-              height={1080}
+              src={cpuI7}
+              width={2500}
+              height={2500}
               alt="forest img"
-              className='flex w-auto h-auto m-auto border border-red-600/40 object-cover'
+              className='flex w-auto h-auto m-auto border border-slate-600/40 object-cover rounded-lg'
             />
           </div>
 
@@ -263,9 +319,11 @@ export default function Home() {
         </div>
 
         <div className='flex flex-col w-full bg-slate-900 p-3'>
+
             <h2 className='w-[800px] text-2xl mx-auto p-4'>
               Last title
             </h2>
+
             <p className='w-[800px] text-blue-300 text-justify mx-auto mb-4 px-4 rounded-lg'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
@@ -274,6 +332,7 @@ export default function Home() {
               fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
               culpa qui officia deserunt mollit anim id est laborum.
             </p>
+
         </div>
 
       </main>
