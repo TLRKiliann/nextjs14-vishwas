08745f500e34db7b51a88a1a72f5911e1d862163
TLRKiliann <philogenie@protonmail.com>
2023-11-27 18:14:20 +0100

    new shadow inner created

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -9,6 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       boxShadow: {
+        'out': '0px 0px 14px rgba(96, 165, 250, 0.8)',
+        'in': 'inset 0px 0px 14px rgba(96, 165, 250, 0.8)',
         'xl': '0px 0px 10px rgba(0, 0, 0, 0.5)',
         '2xl': '0px 0px 20px rgba(0, 0, 0, 1)',
         '3xl': '0px 0px 30px rgba(0, 0, 0, 1)',
