30fe330e8c171acd9f09d8a53f607ce07582e6fe
TLRKiliann <philogenie@protonmail.com>
2023-12-01 12:16:11 +0100

    new animation added & some changes

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -11,18 +11,30 @@ const config: Config = {
       keyframes: {
         sliceappear: {
           "0%": {
-            opacity: "0.5"
+            opacity: "0"
           },
           "100%": {
+            opacity: "1"
+          }
+        },
+        upstart: {
+          '0%': { 
+            transform: 'translateY(200px)',
+            opacity: "0"
+          },
+          '100%': { 
+            transform: 'translateY(0px)',
             opacity: "1"
           }
         }
       },
       animation: {
         "slice-appear": "sliceappear 0.4s ease-in-out 1",
+        "up-start": "upstart 0.4s ease-in-out 1",
       },
       dropShadow: {
         'lg': '2px 2px 1px rgba(0, 0, 0, 0.4)',
+        'lgwhite': '2px 2px 1px rgba(255, 255, 255, 1)',
       },
       boxShadow: {
         'btn': '0px 0px 7px rgba(40, 40, 40, 0.8)',
