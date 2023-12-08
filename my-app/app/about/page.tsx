d45c2252be7f4b19bd6891f608630719dbcb8195
TLRKiliann <philogenie@protonmail.com>
2023-12-08 18:25:11 +0100

    about created

---
+++ my-app/app/about/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="min-h-screen">
+
+      <h1 className='text-4xl font-bold 
+        text-transparent bg-clip-text 
+        dark:bg-gradient-to-br dark:from-indigo-500 dark:from-10% 
+        dark:via-sky-500 dark:via-50% dark:to-emerald-500 dark:to-90%
+        bg-gradient-to-br from-violet-500 from-10% 
+        via-purple-500 via-50% to-violet-200 to-90%
+        m-auto p-4'
+      >
+        About US
+      </h1>
+    </div>
+  )
+}
