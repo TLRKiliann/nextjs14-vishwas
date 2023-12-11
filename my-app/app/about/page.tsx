f3b309bda0bb7f34dfd3ae35ae6eaecef1359ca0
TLRKiliann <philogenie@protonmail.com>
2023-12-11 12:22:04 +0100

    our story page created

--- my-app/app/about/page.tsx
+++ my-app/app/about/page.tsx
@@ -1,19 +1,199 @@
 import React from 'react'
-
+import Image from 'next/image';
+import story from '@/public/img_bg/wallpaper-about.jpg'
 export default function About() {
   return (
     <div className="min-h-screen">
 
-      <h1 className='text-4xl font-bold 
-        text-transparent bg-clip-text 
-        dark:bg-gradient-to-br dark:from-indigo-500 dark:from-10% 
-        dark:via-sky-500 dark:via-50% dark:to-emerald-500 dark:to-90%
-        bg-gradient-to-br from-violet-500 from-10% 
-        via-purple-500 via-50% to-violet-200 to-90%
-        m-auto p-4'
-      >
-        About US
-      </h1>
+      <div className='fixed -z-10'>
+        <Image
+          src={story}
+          width={1920}
+          height={1080}
+          alt="our story img"
+          className='object-cover'
+        />
+      </div>
+
+      <section className='md:w-4/5 xl:w-3/5 m-auto bg-slate-100/80'>
+        <article>
+          <h1 className='text-3xl 
+            text-transparent bg-clip-text 
+            dark:bg-gradient-to-br dark:from-indigo-500 dark:from-10% 
+            dark:via-sky-500 dark:via-50% dark:to-emerald-500 dark:to-90%
+            bg-gradient-to-br from-violet-500 from-10% 
+            via-purple-500 via-50% to-violet-200 to-90%
+            text-center pt-10'
+          >
+            Our story
+          </h1>
+          <h2 className='text-lg font-bold italic
+            text-transparent bg-clip-text 
+            dark:bg-gradient-to-br dark:from-indigo-500 dark:from-10% 
+            dark:via-sky-500 dark:via-50% dark:to-emerald-500 dark:to-90%
+            bg-gradient-to-br from-violet-500 from-10% 
+            via-purple-500 via-50% to-violet-200 to-90%
+            text-center my-4'
+          >
+            A Luxurious Experience
+          </h2>
+          <p className='text-slate-600 
+            text-justify
+            px-20 pb-3'
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+
+          <p className='text-slate-600 
+            text-justify
+            px-20 pb-8'
+          >
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </article>
+
+        <article>
+          <h2 className='text-lg font-bold italic
+            text-transparent bg-clip-text 
+            dark:bg-gradient-to-br dark:from-indigo-500 dark:from-10% 
+            dark:via-sky-500 dark:via-50% dark:to-emerald-500 dark:to-90%
+            bg-gradient-to-br from-violet-500 from-10% 
+            via-purple-500 via-50% to-violet-200 to-90%
+            text-center mb-4'
+          >
+            Article 1
+          </h2>
+          <p className='text-slate-600 
+            text-justify
+            px-20 pb-3'
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+
+          <p className='text-slate-600 
+            text-justify
+            px-20 pb-8'
+          >
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </article>
+
+        <article>
+          <h2 className='text-lg font-bold italic
+            text-transparent bg-clip-text 
+            dark:bg-gradient-to-br dark:from-indigo-500 dark:from-10% 
+            dark:via-sky-500 dark:via-50% dark:to-emerald-500 dark:to-90%
+            bg-gradient-to-br from-violet-500 from-10% 
+            via-purple-500 via-50% to-violet-200 to-90%
+            text-center mb-4'
+          >
+            Article 2
+          </h2>
+          <p className='text-slate-600 
+            text-justify
+            px-20 pb-3'
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </p>
+
+          <p className='text-slate-600 
+            text-justify
+            px-20 pb-8'
+          >
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </article>
+
+        <article>
+          <h2 className='text-lg font-bold italic
+            text-transparent bg-clip-text 
+            dark:bg-gradient-to-br dark:from-indigo-500 dark:from-10% 
+            dark:via-sky-500 dark:via-50% dark:to-emerald-500 dark:to-90%
+            bg-gradient-to-br from-violet-500 from-10% 
+            via-purple-500 via-50% to-violet-200 to-90%
+            text-center mt-0'
+          >
+            Article 3
+          </h2>
+          <p className='text-slate-600 
+            text-justify          
+            px-20 pt-4 pb-8'
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </article>
+
+        <div>
+          
+          <h2>Our partners</h2>
+          
+          <div className="flex bg-slate-600">
+
+            <div className='flex align-center'>
+              {/* <div>
+                <Image src={} width={} height={} alt="" className='' />
+              </div> */}
+
+              {/* <div>
+                <Image src={} width={} height={} alt="" className='' />
+              </div> */}
+
+              {/* <div>
+                <Image src={} width={} height={} alt="" className='' />
+              </div> */}
+            </div>
+
+            <div>
+              {/* <div>
+                <Image src={} width={} height={} alt="" className='' />
+              </div> */}
+
+              {/* <div>
+                <Image src={} width={} height={} alt="" className='' />
+              </div> */}
+
+              {/* <div>
+                <Image src={} width={} height={} alt="" className='' />
+              </div> */}
+            </div>
+
+            <div>
+              {/* <div>
+                <Image src={} width={} height={} alt="" className='' />
+              </div> */}
+
+              {/* <div>
+                <Image src={} width={} height={} alt="" className='' />
+              </div> */}
+
+              {/* <div>
+                <Image src={} width={} height={} alt="" className='' />
+              </div> */}
+            </div>
+
+          </div>
+        
+        </div>
+
+      </section>
+
     </div>
   )
 }
