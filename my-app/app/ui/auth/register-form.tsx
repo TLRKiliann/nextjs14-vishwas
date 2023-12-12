e6e6964dd9221fa1be4b7c49bf592b700ab1ea27
TLRKiliann <philogenie@protonmail.com>
2023-12-12 14:22:18 +0100

    register created to test server action

---
+++ my-app/app/ui/auth/register-form.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import React from 'react'
+import type { Metadata } from 'next'
+import { useFormState, useFormStatus } from 'react-dom'
+import mysqlServerAction from '@/app/lib/actions'
+
+type PropsinitialState = {
+    message?: string;
+}
+
+const initialState = {
+  message: undefined,
+}
+
+export const metadata: Metadata = {
+  title: "register",
+  description: "registered"
+}
+
+export default function RegisterForm() {
+
+    const {pending} = useFormStatus()
+
+    const [state, formAction] = useFormState<PropsinitialState>(mysqlServerAction, initialState)
+
+    return (
+        <form action={formAction} className='flex flex-col w-2/5 m-auto bg-blue-600 p-10'>
+
+            <h2>Register</h2>
+
+            <label htmlFor="id">Id</label>
+            <input type="text" id="id" name="id" />
+
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name" />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
+
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" />
+
+            <button type="submit" id="submit" name="submit" value="insert"
+                className='bg-red-600 mt-4'>
+            {pending ? "Pending..." : "Submit"}
+            </button>
+            {state?.message ? (
+                <p className='text-md text-center text-orange-500 mt-4'>{state.message}</p> 
+                ) : (
+                <p className='text-md text-center text-green-400 mt-4'>Insert data into fields</p>
+                )}
+        </form>
+    )
+}
