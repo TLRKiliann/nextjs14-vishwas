948411c87151b154085862215c1522a3c21d569f
TLRKiliann <philogenie@protonmail.com>
2023-12-23 16:30:45 +0100

    main page of products created

--- my-app/app/products/page.tsx
+++ my-app/app/products/page.tsx
@@ -1,21 +1,71 @@
-import { genericQuery } from '@/app/lib/db';
-import DropDownMenu from '@/app/ui/products/dropdown-menu';
-import DisplayerProducts from "@/app/ui/products/displayer-products";
+import MainShop from '@/app/ui/products/main-shop';
 
-export default async function ListProducts() {
+type TitleProps = {
+    id: number;
+    title: string;
+}
 
-    const data: unknown = await genericQuery("SELECT * FROM products", []);
-    const products: string = JSON.stringify(data);
+type LinksProps = {
+    id: number;
+    link: string;
+}
+
+export default function ListProducts() {
+
+    const titles: TitleProps[] = [
+        {
+            id: 1,
+            title: "Decks"
+        },
+        {
+            id: 2,
+            title: "Wheels"
+        },
+        {
+            id: 3,
+            title: "Trucks"
+        }
+    ];
+
+    const links: LinksProps[] = [
+        {
+            id: 1,
+            link: "/products/decks"},
+        {
+            id: 2,
+            link: "/products/wheels"},
+        {
+            id: 3,
+            link: "/products/trucks"}
+    ];
 
     return (
         <div className="min-h-screen bg-slate-100 dark:bg-slate-900 py-[75px]">
+
             <div>
-                <DropDownMenu products={JSON.parse(products)} />
+                <h1 className='text-4xl font-bold 
+                    text-transparent bg-clip-text
+                    dark:bg-gradient-to-br dark:from-indigo-500 dark:from-10% 
+                    dark:via-sky-500 dark:via-50% dark:to-emerald-500 dark:to-90%
+                    bg-gradient-to-br from-violet-500 from-10% 
+                    via-purple-500 via-50% to-violet-200 to-90%
+                    p-4'
+                >
+                    Welcome to shop !
+                </h1>
             </div>
-               
-            <div className='px-20 pt-2 pb-10'>
-                <DisplayerProducts products={JSON.parse(products)} />
-            </div>
+
+            {titles.map((tName: TitleProps) => (
+                links.map((lName: LinksProps) => {
+                    if (lName.id === tName.id) {
+                        return (
+                            <MainShop key={tName.id} link={lName.link}>
+                                {tName.title}
+                            </MainShop>
+                        )
+                    }
+                })
+            ))}
             
         </div>
     )
---
+++ my-app/app/ui/products/main-shop.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import deckProfile from '@/public/img_decks/deck-h.png';
+import deckHorizon from '@/public/img_decks/deck2-h.png';
+
+type ShopProps = {
+    link: string;
+    children: React.ReactNode;
+}
+
+export default function MainShop({link, children}: ShopProps) {
+
+    const [deckBool, setDeckBool] = useState<boolean>(false);
+
+    const handleDecks = () => {
+        setDeckBool((deckBool) => !deckBool)
+    }
+
+    return (
+        <div className='flex'>
+
+            {deckBool === false ? (
+                <div className='flex flex-col items-center justify-center w-full h-[340px]'>
+
+                    <Link
+                        href={`${link}`}
+                        onClick={handleDecks}  
+                        className='relative transform ease-in-out duration-300 active:rotate-x-[180deg] z-0'
+                    >
+
+                        <h2 
+                            className='absolute flex items-center justify-center w-full h-full 
+                                text-5xl font-bold text-transparent bg-clip-text
+                                bg-gradient-to-t from-fuchsia-700 from-30% 
+                                to-yellow-400 to-70% animate-scale-decks z-10'>
+                            {children}
+                        </h2>
+
+                        <Image
+                            src={deckProfile}
+                            width={400}
+                            height={200}
+                            alt="img deck-profile"
+                            className="object-cover"
+                        />
+                    </Link>
+                </div>
+            ) : (
+                <div className='flex items-center justify-center w-full h-[340px]'>
+                    <Image
+                        src={deckHorizon}
+                        width={400}
+                        height={200}
+                        alt="img deck-horizon"
+                        className="object-cover"
+                    />
+                </div>
+            )}
+        </div>
+    )
+}
