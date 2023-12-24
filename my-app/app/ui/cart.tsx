e4e30c02140796be82f1a032ca9fc672fe24597c
TLRKiliann <philogenie@protonmail.com>
2023-12-24 14:30:05 +0100

    cart at work

--- my-app/app/ui/cart.tsx
+++ my-app/app/ui/cart.tsx
@@ -12,7 +12,8 @@ export default function Cart() {
     const pathname = usePathname();
     
     const {state} = useCart();
-    console.log(state.items)
+
+    console.log(state.items, "+ state.items")
 
     const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -34,51 +35,77 @@ export default function Cart() {
                 <BsCart3 size={22} />
                 <div className='absolute text-sm m-auto pl-4 -mt-1'>
                     <p className='flex items-center justify-center w-6 h-6 
-                        border border-slate-950 hover:border-slate-50 dark:border-slate-50 rounded-full'
+                        text-slate-900 bg-slate-50 border-none 
+                        hover:border-slate-50 dark:border-slate-50 rounded-full'
                     >
                         {state.items.length}
                     </p>
                 </div>
             </span>
 
-
-
-
-
-
-            <div className={`absolute flex-col text-md font-normal top-0 right-0 w-[400px] h-[auto] 
-                text-slate-900 bg-cyan-50 rounded-br-lg rounded-bl-lg ${transClass} z-20`}
+            <div className={`absolute flex-col text-md font-normal top-0 right-0 w-[500px] h-[auto] 
+                text-slate-900 bg-slate-100 rounded-br-lg rounded-bl-lg ${transClass} z-20`}
             >
                 <div className='text-right p-2'>
                     <button
                         type="button"
                         onClick={toggle} 
-                        className='w-7 h-7 border border-slate-900 rounded-full pb-2'>
+                        className='w-7 h-7 bg-slate-100 border border-slate-900 
+                            hover:text-slate-600 hover:bg-slate-200 hover:border-slate-200
+                             active:text-slate-50 active:bg-slate-400
+                            pb-2 rounded-full'>
                         x
                     </button>
                 </div>
 
-                <div className='flex items-center justify-between pb-4 border border-slate-900'>
+                <div className='my-auto border border-slate-900'>
 
-                    {state.items.map((s) => (
-                        <p key={s.id}>deckname{s.deckname}</p>   
+                    {state.items.map((item) => (
+                        <div key={item.id} className='flex items-center justify-around my-2'>
+
+                            <div className='border border-slate-900 m-auto ml-4'>
+                                
+                                <Image src={item.img} width={100} height={100} alt={item.img} />
+                            </div>
+
+                            <div className='flex-1 items-center m-auto mr-4 border border-slate-900 ml-4'>
+                                <p className='text-xl font-bold text-slate-600 border 
+                                    border-slate-900 mb-2'>
+                                    {item.deckname.toUpperCase()} 
+                                </p>
+                                <p className='text-lg text-slate-600 border border-slate-900 mb-2'>
+                                    Price: {item.price.toFixed(2)}.- CHF
+                                </p>
+                                <div className='flex items-center justify-between'>
+                                    <p className='text-md text-slate-600 border border-slate-900'>
+                                        stock: {item.stock}
+                                    </p>
+                                    <span className='flex items-center justify-between
+                                        border border-slate-900'>
+                                        <button className='w-8 h-8 mx-4 text-slate-200 font-bold 
+                                            bg-red-500 border-none hover:text-slate-300 
+                                                hover:bg-red-600 active:text-slate-50 active:bg-red-400 rounded-full'>
+                                                -
+                                        </button>
+
+                                        <button className='w-8 h-8 mx-4 text-slate-200 font-bold 
+                                            bg-red-500 border-none hover:text-slate-300 
+                                                hover:bg-red-600 active:text-slate-50 active:bg-red-400 rounded-full'>
+                                                +
+                                        </button>
+                                    </span>
+                                </div>
+                            </div>
+
+                        </div>
                     ))}
-
-                    <div className='border border-slate-900 m-auto ml-4'>
-                        Image
-                        {/* <Image src={} width={100} height={100} alt={} /> */}
-                    </div>
-
-                    <div className='flex-1 items-center m-auto mr-4 border border-slate-900'>
-                        <p className='text-xl text-blue-600 border border-slate-900 mb-2'>Name: .... </p>
-                        <p className='text-lg text-blue-600 border border-slate-900 mb-2'>Price: ...</p>
-                        <p className='text-md text-blue-600 border border-slate-900'>stock: ...</p>
-                    </div>
 
                 </div>
 
                 <div className='flex items-center justify-center py-4 border border-slate-900'>
-                    <Link href="/cart" className='text-slate-50 bg-sky-700/80 hover:bg-sky-700/60
+                    <Link href="/cart" 
+                        onClick={toggle} 
+                        className='text-slate-50 bg-sky-700/80 hover:bg-sky-700/60
                         hover:shadow-none active:text-slate-50 active:bg-sky-700/40 
                         active:scale-95 active:shadow-none border-none px-4 py-1 rounded-lg shadow-md'>
                         Go to cart
