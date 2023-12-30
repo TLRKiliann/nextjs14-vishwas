aec08b451a67a88915243477fc790d758ee0ff4a
TLRKiliann <philogenie@protonmail.com>
2023-12-30 15:10:14 +0100

    new img of baker's decks added

--- my-app/app/lib/datas.ts
+++ my-app/app/lib/datas.ts
@@ -5,7 +5,25 @@ import bakerUnder from '@/public/img_decks/baker/baker-under.jpg';
 import bakerProfile from '@/public/img_decks/baker/baker-profile.jpg';
 import bakerOver_2 from '@/public/img_decks/baker/baker2-over.jpg';
 import bakerUnder_2 from '@/public/img_decks/baker/baker2-under.jpg';
-import bakerProfile_3 from '@/public/img_decks/baker/baker2-profile.jpg';
+import bakerProfile_2 from '@/public/img_decks/baker/baker2-profile.jpg';
+import bakerOver_3 from '@/public/img_decks/baker/baker3-over.jpg';
+import bakerUnder_3 from '@/public/img_decks/baker/baker3-under.jpg';
+import bakerProfile_3 from '@/public/img_decks/baker/baker3-profile.jpg';
+import bakerOver_4 from '@/public/img_decks/baker/baker4-over.jpg';
+import bakerUnder_4 from '@/public/img_decks/baker/baker4-under.jpg';
+import bakerProfile_4 from '@/public/img_decks/baker/baker4-profile.jpg';
+import bakerOver_5 from '@/public/img_decks/baker/baker5-over.jpg';
+import bakerUnder_5 from '@/public/img_decks/baker/baker5-under.jpg';
+import bakerProfile_5 from '@/public/img_decks/baker/baker5-profile.jpg';
+import bakerOver_6 from '@/public/img_decks/baker/baker6-over.jpg';
+import bakerUnder_6 from '@/public/img_decks/baker/baker6-under.jpg';
+import bakerProfile_6 from '@/public/img_decks/baker/baker6-profile.jpg';
+import bakerOver_7 from '@/public/img_decks/baker/baker7-over.jpg';
+import bakerUnder_7 from '@/public/img_decks/baker/baker7-under.jpg';
+import bakerProfile_7 from '@/public/img_decks/baker/baker7-profile.jpg';
+import bakerOver_8 from '@/public/img_decks/baker/baker8-over.jpg';
+import bakerUnder_8 from '@/public/img_decks/baker/baker8-under.jpg';
+import bakerProfile_8 from '@/public/img_decks/baker/baker8-profile.jpg';
 
 export const imgSet: ImgSetProps[] = [
     {
@@ -14,7 +32,35 @@ export const imgSet: ImgSetProps[] = [
     },
     {
         id: 2,
-        images: [bakerOver_2, bakerUnder_2, bakerProfile_3]
+        images: [bakerOver_2, bakerUnder_2, bakerProfile_2]
+    },
+    {
+        id: 3,
+        images: [bakerOver_3, bakerUnder_3, bakerProfile_3]
+    },
+    {
+        id: 4,
+        images: [bakerOver_4, bakerUnder_4, bakerProfile_4]
+    },
+    {
+        id: 5,
+        images: [bakerOver_5, bakerUnder_5, bakerProfile_5]
+    },
+    {
+        id: 6,
+        images: [bakerOver_6, bakerUnder_6, bakerProfile_6]
+    },
+    {
+        id: 7,
+        images: [bakerOver_7, bakerUnder_7, bakerProfile_7]
+    },
+    {
+        id: 8,
+        images: [bakerOver_8, bakerUnder_8, bakerProfile_8]
+    },
+    {
+        id: 9,
+        images: [bakerOver_8, bakerUnder_8, bakerProfile_8]
     }
 ];
 
