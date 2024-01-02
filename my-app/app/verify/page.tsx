e6cb29b4943e55e7e3356e25ddc697e38bab8ca0
TLRKiliann <philogenie@protonmail.com>
2024-01-02 15:52:41 +0100

    route to test

---
+++ my-app/app/verify/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from "next/image"
+import spinLoader from "@/public/img_logo/loader.png";
+
+export default function Loading() {
+  return (
+    <div className="w-full min-h-screen">
+        <div className='w-[200px] flex items-center p-4 mt-[100px]'>
+
+          <Image
+              src={spinLoader}
+              width={20}
+              height={20}
+              alt="img loader"
+              className="object-cover animate-spin" 
+          />
+
+          <h2 className='font-bold ml-4'>Loading...</h2>
+
+        </div>
+    </div>
+  )
+}
