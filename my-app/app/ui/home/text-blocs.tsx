fb621eab82d1215e37212c0397ad69e58610aa45
TLRKiliann <philogenie@protonmail.com>
2024-01-02 14:25:04 +0100

    title & txt resized

--- my-app/app/ui/home/text-blocs.tsx
+++ my-app/app/ui/home/text-blocs.tsx
@@ -14,16 +14,15 @@ export default function TextBlocks({title, text, children}: PropsText) {
 
             <div className='flex flex-col align-center justify-center w-4/5 m-auto'>
 
-                <h2 className='text-3xl font-extrabold
-                    text-transparent bg-clip-text 
+                <h2 className='sm:text-3xl xl:text-5xl font-extrabold text-transparent bg-clip-text 
                     dark:bg-gradient-to-br dark:from-indigo-500 dark:from-10% 
                     dark:via-sky-500 dark:via-50% dark:to-emerald-500 dark:to-90%
-                    bg-indigo-500 mb-4'
+                    bg-indigo-500 mb-4 py-4'
                 >
                     {title}
                 </h2>
 
-                <p className='text-justify text-slate-600 dark:text-blue-300'>
+                <p className='sm:text-md xl:text-2xl text-justify text-slate-600 dark:text-blue-300'>
                     {text}
                 </p>
 
