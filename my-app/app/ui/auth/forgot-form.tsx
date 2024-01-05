db843f4b0a18b98552a1186ee05fdace6e81ed3e
TLRKiliann <philogenie@protonmail.com>
2024-01-05 15:57:51 +0100

    design improved

--- my-app/app/ui/auth/forgot-form.tsx
+++ my-app/app/ui/auth/forgot-form.tsx
@@ -12,26 +12,23 @@ export default function ForgetForm() {
     return (
         <form
             action={formData}
-            className='flex flex-col w-2/5 text-md bg-slate-50 px-8 py-7 rounded-lg shadow-lg'
+            className='flex flex-col w-2/5 text-md bg-slate-50 p-6 rounded-lg shadow-lg'
         >
             <label
                 htmlFor="email"
-                className='text-slate-600 font-bold'
-            >
+                className='text-xl font-bold text-transparent bg-clip-text dark-title-h1 light-title-h1 mx-4 mb-1'>
                 Email :
             </label>
             <input
                 type="email"
                 id="email"
                 name="email"
-                placeholder='Enter your email here'
+                placeholder='Enter your email'
                 required
-                className='text-slate-900
-                    bg-slate-100
-                    hover:bg-slate-300/60
-
-                    focus:outline-none focus:ring focus:ring-slate-500/50
-                    rounded px-2 py-1 mt-2 mb-6 shadow-ingray'
+                className='text-lg text-violet-600 dark:text-cyan-400 bg-violet-100 hover:bg-violet-200 
+                active:bg-white dark:bg-slate-200 dark:hover:bg-slate-300 dark:active:bg-sky-800
+                focus:outline-none focus:ring focus:ring-violet-400/70 dark:focus:ring-sky-500
+                mx-4 pl-2 py-1 rounded-lg'
             />
             <button
                 type="submit"
@@ -44,7 +41,7 @@ export default function ForgetForm() {
                     hover:shadow-none
                     active:text-slate-400
                     active:bg-slate-100
-                    px-5 py-2
+                    mx-4 mt-6 py-2
                     rounded-lg shadow-lg'
             >
                 {pending ? "Pending..." : "Submit"}
