b3e32f57f1174fe4bdf6554159cbf3bd3a689635
TLRKiliann <philogenie@protonmail.com>
2024-01-12 15:18:37 +0100

    error & not-found setup done

---
+++ my-app/app/products/wheels/slimballs/[wheelId]/not-found.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className='w-full min-h-screen flex flex-col align-center justify-center'>
+      <div className='m-auto'>
+        <h2 className='text-3xl font-bold mb-2'>
+          Page not found...
+        </h2>
+        <Link 
+          href="/products/wheels" 
+          className='dark:text-sky-500 dark:hover:text-sky-600 dark:active:text-sky-400
+            text-purple-500 hover:text-purple-600 active:text-purple-400'>
+          Back to wheels
+        </Link>
+      </div>
+    </div>
+  )
+}
