3dc36ef11a0b7bd4a0e9912a9db86e199463c8b7
TLRKiliann <philogenie@protonmail.com>
2024-01-14 14:29:23 +0100

    restructured component by family wheels

--- my-app/app/products/wheels/page.tsx
+++ my-app/app/products/wheels/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import type { WheelsProps } from '@/app/lib//definitions';
 import React from 'react';
 import { genericQuery } from '@/app/lib/db';
 import WheelCard from '@/app/ui/products/wheels/wheel-card';
@@ -30,12 +31,83 @@ export default async function AxisShop() {
   return (
     <div className='min-h-screen bg-slate-100 dark:bg-slate-900 mt-4'>
       
-      <WheelCard data={JSON.parse(data)} />
+      <div className='xs:w-4/5 w-3/5 h-auto bg-slate-300 m-auto mb-10 pt-4 pb-10 rounded dark:shadow-in 
+          shadow-inviolet'>
+    
+        <div className="w-[600px] m-auto py-2">
+          <h2 className='text-4xl text-transparent bg-clip-text dark-title-h1 light-title-h1'>
+            Bones
+          </h2>
+        </div>
 
-      <WheelCard data={JSON.parse(dataTwo)} />
+        <div className='md:flex md:flex-col lg:grid grid-cols-4 grid-row-3 gap-0 justify-items-center 
+          md:w-[150px] lg:w-[600px] bg-slate-50 m-auto rounded-xl dark:shadow-lg 
+          shadow-none'>
 
-      <WheelCard data={JSON.parse(dataThree)} />
+          {JSON.parse(data).map((wheelUnit: WheelsProps) => (
+            <WheelCard
+              key={wheelUnit.id}
+              id={wheelUnit.id}
+              name={wheelUnit.name}
+              price={wheelUnit.price}
+              stock={wheelUnit.stock}
+              img={wheelUnit.img}
+            />
+          ))}
+        </div>
+      </div>
 
+      <div className='xs:w-4/5 w-3/5 h-auto bg-slate-300 m-auto mb-10 pt-4 pb-10 rounded dark:shadow-in 
+          shadow-inviolet'>
+    
+        <div className="w-[600px] m-auto py-2">
+          <h2 className='text-4xl text-transparent bg-clip-text dark-title-h1 light-title-h1'>
+            Slimballs
+          </h2>
+        </div>
+
+        <div className='md:flex md:flex-col lg:grid grid-cols-4 grid-row-3 gap-0 justify-items-center 
+          md:w-[150px] lg:w-[600px] bg-slate-50 m-auto rounded-xl dark:shadow-lg 
+          shadow-none'>
+
+          {JSON.parse(dataTwo).map((wheelUnit: WheelsProps) => (
+            <WheelCard
+              key={wheelUnit.id}
+              id={wheelUnit.id}
+              name={wheelUnit.name}
+              price={wheelUnit.price}
+              stock={wheelUnit.stock}
+              img={wheelUnit.img}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className='xs:w-4/5 w-3/5 h-auto bg-slate-300 m-auto mb-10 pt-4 pb-10 rounded dark:shadow-in 
+          shadow-inviolet'>
+    
+        <div className="w-[600px] m-auto py-2">
+          <h2 className='text-4xl text-transparent bg-clip-text dark-title-h1 light-title-h1'>
+            Spitfire
+          </h2>
+        </div>
+
+        <div className='md:flex md:flex-col lg:grid grid-cols-4 grid-row-3 gap-0 justify-items-center 
+          md:w-[150px] lg:w-[600px] bg-slate-50 m-auto rounded-xl dark:shadow-lg 
+          shadow-none'>
+
+          {JSON.parse(dataThree).map((wheelUnit: WheelsProps) => (
+            <WheelCard
+              key={wheelUnit.id}
+              id={wheelUnit.id}
+              name={wheelUnit.name}
+              price={wheelUnit.price}
+              stock={wheelUnit.stock}
+              img={wheelUnit.img}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
--- my-app/app/ui/products/wheels/wheel-card.tsx
+++ my-app/app/ui/products/wheels/wheel-card.tsx
@@ -1,14 +1,15 @@
 "use client";
 
+import type { WheelsProps } from '@/app/lib//definitions';
 import React from 'react'
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import type { WheelsProps } from '@/app/lib//definitions';
 import { useFormState, useFormStatus } from 'react-dom';
 import { queryWheelsCart } from '@/app/lib/actions';
 import { useShoppingCart } from '@/app/context/cart-context';
+import BtnRemoveAll from './btn-remove-all';
 
-export default function WheelCard({data}: {data: WheelsProps[]}) {
+export default function WheelCard({id, name, price, stock, img}: WheelsProps) {
 
     const router = useRouter();
 
@@ -18,14 +19,14 @@ export default function WheelCard({data}: {data: WheelsProps[]}) {
     const {
         getItemQuantity,
         increaseCartQuantity,
-        decreaseCartQuantity,
-        //removeFromCart
+        decreaseCartQuantity
     } = useShoppingCart();
 
-    const mappingData = data.find((d: WheelsProps) => d.id);
+    const quantity: number = getItemQuantity(id);
 
-    let quantity: number = getItemQuantity(Number(mappingData?.id));
-    
+    const wordCut = name.split(" ");
+    const wheelPath = wordCut?.[0];
+
     const handleDelete = (id: number, name: string, price: number, img: string, stock: number) => {
         decreaseCartQuantity(id, name, price, img, stock);
     };
@@ -33,89 +34,67 @@ export default function WheelCard({data}: {data: WheelsProps[]}) {
     const handleAdd = (id: number, name: string, price: number, img: string, stock: number) => {
         increaseCartQuantity(id, name, price, img, stock);
     };
-    const bonesWord = data.find((d) => d.name);
-    const convertBonesWord: string | undefined = bonesWord?.name;
-    const wordCut = convertBonesWord?.split(" ");
-    const wheelPath = wordCut?.[0];
 
     const handlePath = (id: number) => {
         router.push(`/products/wheels/${wheelPath}/${id}`);
     };
 
-    if (!data) {
-        throw new Error("Error: server failed !");
-    };
-
     return (
-        <div className='xs:w-4/5 w-3/5 h-auto bg-slate-300 m-auto mb-10 pt-4 pb-10 rounded dark:shadow-in 
-            shadow-inviolet'>
-    
-            <div className="w-[600px] m-auto py-2">
-                <h2 className='text-4xl text-transparent bg-clip-text dark-title-h1 light-title-h1'>
-                    {wheelPath ? wheelPath.charAt(0).toUpperCase() + wheelPath.slice(1) : null}
-                </h2>
+        <div key={id} className='w-[150px] h-auto'>
+            
+            <span onClick={() => handlePath(id)}>
+                <Image src={img} width={435} height={580} alt="img wheel"
+                    className='object-cover cursor-pointer hover:opacity-90 rounded-tr-xl rounded-tl-xl' />
+            </span>
+
+            <div className='text-slate-600/80 p-2'>
+                <h3 className='text-sm font-bold'>
+                    {name.charAt(0).toUpperCase() + name.slice(1)}
+                </h3>
+                <h4 className='text-sm font-bold'>{price}.-</h4>
+                <p className='text-sm mt-2'>Stock: {stock - quantity}pcs</p>
             </div>
 
-            <div className='md:flex md:flex-col lg:grid grid-cols-4 grid-row-3 gap-0 justify-items-center 
-                md:w-[150px] lg:w-[600px] bg-slate-50 m-auto rounded-xl dark:shadow-lg 
-                shadow-none'>
+            <p className='text-center text-lg font-normal text-slate-500/80 m-auto pt-2'>
+                <span className={`font-bold ${quantity === 0 ? "text-slate-500/80" : "text-red-500/80" }`}>
+                    {quantity}&nbsp;
+                </span>
+                in cart
+            </p>
 
-                {data.map((wheelUnit: WheelsProps) => (
-                    <span key={wheelUnit.id} 
-                        className='w-[150px] h-auto'>
-                        <span onClick={() => handlePath(wheelUnit.id)}>
-                            <Image src={wheelUnit.img} width={435} height={580} alt="img wheelUnit"
-                                className='object-cover cursor-pointer hover:opacity-90 
-                                    rounded-tr-xl rounded-tl-xl' />
-                        </span>
-                        <div className='text-slate-600/80 p-2'>
-                            <h3 className='text-sm font-bold'>
-                                {wheelUnit.name.charAt(0).toUpperCase() + wheelUnit.name.slice(1)}
-                            </h3>
-                            <h4 className='text-sm font-bold'>{wheelUnit.price}.-</h4>
-                            <p className='text-sm mt-2'>Stock: {wheelUnit.stock - quantity}pcs</p>
-                        </div>
+            <form action={formAction}>
 
-                        <p className='text-center text-lg font-normal text-slate-500/80 m-auto pt-2'>
-                            <span className={`font-bold ${quantity === 0 ? "text-slate-500/80" : "text-red-500/80" }`}>
-                                {quantity}&nbsp;
-                            </span>
-                            in cart
-                        </p>
+                <input type="number" id="id" name="id" value={id} hidden readOnly />
+                <input type="text" id="name" name="name" value={name} hidden readOnly />
+                <input type="number" id="price" name="price" value={price} hidden readOnly />
+                <input type="number" id="count" name="count" value={quantity} hidden readOnly />
+                <input type="number" id="stock" name="stock" value={stock} hidden readOnly />
+                <input type="text" id="img" name="img" value={img} hidden readOnly />
 
-                        <form action={formAction} className="flex justify-between py-2">
+                <div className="flex items-center justify-between px-4 py-2">
+                    <button type="submit" id="submit" name="submit" value="deleteWheel"
+                        onClick={() => handleDelete(id, name, price, 
+                            img, stock)}
+                        disabled={pending} className='button-wheels'>
+                        {pending ? "Pending..." : "-"}
+                    </button>
 
-                            <input type="number" id="id" name="id" value={wheelUnit.id} hidden readOnly />
-                            <input type="text" id="name" name="name" value={wheelUnit.name} hidden readOnly />
-                            <input type="number" id="price" name="price" value={wheelUnit.price} hidden readOnly />
-                            <input type="number" id="count" name="count" value={quantity} hidden readOnly />
-                            <input type="number" id="stock" name="stock" value={wheelUnit.stock} hidden readOnly />
-                            <input type="text" id="img" name="img" value={wheelUnit.img} hidden readOnly />
+                    <button type="submit" id="submit" name="submit" value="addWheel"
+                        onClick={() => handleAdd(id, name, price, 
+                            img, stock)}
+                        disabled={pending} className='button-wheels'>
+                        {pending ? "Pending..." : "+"}
+                    </button>
+                </div>
+                {code?.message && quantity !== 0 ? (
+                    <p className='message-cart'>
+                        {code.message}
+                    </p>
+                ) : null}
+            </form>
 
-                            <button type="submit" id="submit" name="submit" value="deleteWheel"
-                                onClick={() => handleDelete(wheelUnit.id, wheelUnit.name, wheelUnit.price, 
-                                    wheelUnit.img, wheelUnit.stock)}
-                                disabled={pending} className='button-card'>
-                                {pending ? "Pending..." : "Sub"}
-                            </button>
+            <BtnRemoveAll id={id} />
 
-                            <button type="submit" id="submit" name="submit" value="addWheel"
-                                onClick={() => handleAdd(wheelUnit.id, wheelUnit.name, wheelUnit.price, 
-                                    wheelUnit.img, wheelUnit.stock)}
-                                disabled={pending} className='button-card'>
-                                {pending ? "Pending..." : "Add"}
-                            </button>
-
-                        </form>
-                        {code?.message && quantity !== 0 ? (
-                        <p className='message-cart'>
-                            {code.message}
-                        </p>
-                        ) : null}
-                    </span>
-                ))}
-
-            </div>
         </div>
     )
 }
