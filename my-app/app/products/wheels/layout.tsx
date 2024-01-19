021aaf2cc69ef49a12a1d8f4691bdc48a470d2e3
TLRKiliann <philogenie@protonmail.com>
2024-01-19 10:40:00 +0100

    code review

--- my-app/app/products/wheels/layout.tsx
+++ my-app/app/products/wheels/layout.tsx
@@ -7,7 +7,7 @@ import SearchUrl from '@/app/ui/products/search-url';
 export default function LayoutBoardShop({children}: {children: React.ReactNode}) {
     return (
         <div className='bg-slate-100 dark:bg-slate-900 py-[75px]'>
-            <div className='flex items-center justify-center w-full h-auto overflow-hidden'>
+            <div className='flex items-center justify-center w-full h-auto'>
                 <Image
                     src={deckTitle}
                     width={550}
