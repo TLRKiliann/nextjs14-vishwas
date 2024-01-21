223290b4c3acd0bdd185103dbbf47b5b552602dc
TLRKiliann <philogenie@protonmail.com>
2024-01-21 22:08:57 +0100

    data verified & setTimeOut handler added to reset input

--- my-app/app/ui/contact/message.tsx
+++ my-app/app/ui/contact/message.tsx
@@ -9,14 +9,14 @@ export default function Message() {
     const {pending} = useFormStatus();
     const [code, formAction] = useFormState(messageToSend, undefined)
 
-    const [user, setUser] = useState<string>("");
+    const [username, setUserName] = useState<string>("");
     const [email, setEmail] = useState<string>("");
     const [textArea, setTextArea] = useState<string>("");
 
     const handleUser = (event: React.ChangeEvent<HTMLInputElement>): void => {
         let value_user: string = event.target.value;
         console.log(value_user);
-        setUser(value_user);
+        setUserName(value_user);
     }
 
     const handleEmail = (event: React.ChangeEvent<HTMLInputElement>): void => {
@@ -30,6 +30,14 @@ export default function Message() {
         setTextArea(textValue);
     }
 
+    const handleTimer = () => {
+        if (typeof window !== "undefined") {
+            const timer = setTimeout(() => {
+                document.getElementById("resetForgot")?.click();
+            }, 1000)
+        }
+    };
+
     return (
         <form action={formAction}
             className='relative flex flex-col w-2/5 h-fit text-lg font-bold text-indigo-500 dark:text-slate-600 
@@ -38,7 +46,7 @@ export default function Message() {
             <h3 className='text-2xl font-bold pb-4'>Email</h3>
             
             <label className="mt-2 mb-1" htmlFor="username">Username</label>
-            <input type="text" id="username" name="username" value={user}
+            <input type="text" id="username" name="username" value={username}
                 onChange={(event) => handleUser(event)}
                 placeholder='Username' required
                 className='text-slate-500 bg-slate-200 px-2 py-1 rounded' />
@@ -57,14 +65,17 @@ export default function Message() {
                 className='text-slate-500 bg-slate-200 p-2 rounded'>
             </textarea>
             
+            <input type="reset" id="resetForgot" hidden />
+
             <button type="submit" id="submit" name="submit" value="sendmessage"
+                onClick={handleTimer}
                 disabled={pending} className='text-slate-50 bg-blue-500 mt-6 
                 hover:bg-blue-600 active:hover:bg-blue-400 py-2 rounded-lg shadow-lg'
             >
                 {pending ? "Pending..." : "Submit"}
             </button>
             {code?.message ? (
-                <p className='text-center text-green-400'>{code.message}</p>
+                <p className='text-center text-green-400 mt-2'>{code.message}</p>
             ) : null}
 
         </form>
