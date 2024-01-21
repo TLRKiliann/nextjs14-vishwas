843c656c8714d52ca80d61c9188e3eaa2a6891f4
TLRKiliann <philogenie@protonmail.com>
2024-01-21 22:11:11 +0100

    setTimeOut handle added to reset input form

--- my-app/app/ui/auth/forgot-form.tsx
+++ my-app/app/ui/auth/forgot-form.tsx
@@ -9,9 +9,17 @@ export default function ForgetForm() {
     const { pending } = useFormStatus();
     const [state, formData] = useFormState(forgotPasswordServerAction, undefined);
 
+    const handleTimer = () => {
+        if (typeof window !== "undefined") {
+            const timer = setTimeout(() => {
+                document.getElementById("resetMessage")?.click();
+            }, 1000)
+        }
+    };
     return (
         <form
             action={formData}
+            autoComplete='off'
             className='flex flex-col w-2/5 text-md bg-slate-50 p-6 rounded-lg shadow-lg'
         >
             <label
@@ -25,16 +33,21 @@ export default function ForgetForm() {
                 name="email"
                 placeholder='Enter your email'
                 required
-                className='text-lg text-violet-600 dark:text-cyan-400 bg-violet-100 hover:bg-violet-200 
+                className='text-lg text-violet-600 dark:text-slate-600 bg-violet-100 hover:bg-violet-200 
                 active:bg-white dark:bg-slate-200 dark:hover:bg-slate-300 dark:active:bg-sky-800
                 focus:outline-none focus:ring focus:ring-violet-400/70 dark:focus:ring-sky-500
                 mx-4 pl-2 py-1 rounded-lg'
             />
+            
+            <input type="reset" id="resetMessage" hidden />
+
             <button
                 type="submit"
                 id="submit"
                 name="submit"
                 value="btnForgotPassword"
+                onClick={handleTimer}
+                disabled={pending}
                 className='text-sm font-bold text-slate-600 bg-slate-100
                     hover:bg-slate-200
                     hover:text-slate-500
@@ -47,7 +60,7 @@ export default function ForgetForm() {
                 {pending ? "Pending..." : "Submit"}
             </button>
             {state?.message ? (
-                <p>{state.message}</p>
+                <p className='text-center text-green-500 mt-2'>{state.message}</p>
             ): null}
         </form>
     )
