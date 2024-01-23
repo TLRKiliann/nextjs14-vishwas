b4e9561bfb543829f446c2e2357bfbc97f1f55dd
TLRKiliann <philogenie@protonmail.com>
2024-01-23 09:46:07 +0100

    some changes

--- my-app/app/lib/deckList.ts
+++ my-app/app/lib/deckList.ts
@@ -1,4 +1,6 @@
 import type { ImgSetProps } from "./definitions";
+import blackOver from '@/public/img_decks/fake_decks/black-over.png';
+import blackProfile from '@/public/img_decks/fake_decks/black-profile.png';
 import bakerOver from '@/public/img_decks/baker/baker-over.jpg';
 import bakerUnder from '@/public/img_decks/baker/baker-under.jpg';
 import bakerProfile from '@/public/img_decks/baker/baker-profile.jpg';
@@ -8,22 +10,12 @@ import bakerProfile_2 from '@/public/img_decks/baker/baker-profile2.jpg';
 import bakerOver_3 from '@/public/img_decks/baker/baker-over3.jpg';
 import bakerUnder_3 from '@/public/img_decks/baker/baker-under3.jpg';
 import bakerProfile_3 from '@/public/img_decks/baker/baker-profile3.jpg';
-import bakerOver_4 from '@/public/img_decks/baker/baker-over4.jpg';
 import bakerUnder_4 from '@/public/img_decks/baker/baker-under4.jpg';
-import bakerProfile_4 from '@/public/img_decks/baker/baker-profile4.jpg';
-import bakerOver_5 from '@/public/img_decks/baker/baker-over5.jpg';
 import bakerUnder_5 from '@/public/img_decks/baker/baker-under5.jpg';
-import bakerProfile_5 from '@/public/img_decks/baker/baker-profile5.jpg';
-import bakerOver_6 from '@/public/img_decks/baker/baker-over6.jpg';
 import bakerUnder_6 from '@/public/img_decks/baker/baker-under6.jpg';
-import bakerProfile_6 from '@/public/img_decks/baker/baker-profile6.jpg';
-import bakerOver_7 from '@/public/img_decks/baker/baker-over7.jpg';
 import bakerUnder_7 from '@/public/img_decks/baker/baker-under7.jpg';
-import bakerProfile_7 from '@/public/img_decks/baker/baker-profile7.jpg';
-import bakerOver_8 from '@/public/img_decks/baker/baker-over8.jpg';
 import bakerUnder_8 from '@/public/img_decks/baker/baker-under8.jpg';
-import bakerProfile_8 from '@/public/img_decks/baker/baker-profile8.jpg';
-
+import bakerUnder_9 from '@/public/img_decks/baker/baker-under9.jpg';
 import blindUnder_1 from '@/public/img_decks/blind/deck_blind1.jpg';
 import blindUnder_2 from '@/public/img_decks/blind/deck_blind2.jpg';
 import blindUnder_3 from '@/public/img_decks/blind/deck_blind3.jpg';
@@ -35,23 +27,16 @@ import blindUnder_8 from '@/public/img_decks/blind/deck_blind8.jpg';
 import blindUnder_9 from '@/public/img_decks/blind/deck_blind9.jpg';
 import blindOver_1 from '@/public/img_decks/fake_decks/red-over.png';
 import blindOver_2 from '@/public/img_decks/fake_decks/bluemarine-over.png';
-import blindOver_3 from '@/public/img_decks/fake_decks/black-over.png';
 import blindOver_4 from '@/public/img_decks/fake_decks/aqua-over.png';
 import blindOver_5 from '@/public/img_decks/fake_decks/green-over.png';
 import blindOver_6 from '@/public/img_decks/fake_decks/bluemarine-over.png';
-import blindOver_7 from '@/public/img_decks/fake_decks/black-over.png';
-import blindOver_8 from '@/public/img_decks/fake_decks/greyblue-over.png';
-import blindOver_9 from '@/public/img_decks/fake_decks/black-over.png';
+import blindOver_8 from '@/public/img_decks/fake_decks/aqua-over.png';
 import blindProfile_1 from '@/public/img_decks/fake_decks/red-profile.png';
 import blindProfile_2 from '@/public/img_decks/fake_decks/bluemarine-profile.png';
-import blindProfile_3 from '@/public/img_decks/fake_decks/black-profile.png';
 import blindProfile_4 from '@/public/img_decks/fake_decks/aqua-profile.png';
 import blindProfile_5 from '@/public/img_decks/fake_decks/green-profile.png';
 import blindProfile_6 from '@/public/img_decks/fake_decks/bluemarine-profile.png';
-import blindProfile_7 from '@/public/img_decks/fake_decks/black-profile.png';
-import blindProfile_8 from '@/public/img_decks/fake_decks/greyblue-profile.png';
-import blindProfile_9 from '@/public/img_decks/fake_decks/black-profile.png';
-
+import blindProfile_8 from '@/public/img_decks/fake_decks/aqua-profile.png';
 import elementUnder_1 from '@/public/img_decks/element/deck_element1.jpg';
 import elementUnder_2 from '@/public/img_decks/element/deck_element2.jpg';
 import elementUnder_3 from '@/public/img_decks/element/deck_element3.jpg';
@@ -62,24 +47,13 @@ import elementUnder_7 from '@/public/img_decks/element/deck_element7.jpg';
 import elementUnder_8 from '@/public/img_decks/element/deck_element8.jpg';
 import elementUnder_9 from '@/public/img_decks/element/deck_element9.jpg';
 import elementOver_1 from '@/public/img_decks/fake_decks/bluemarine-over.png';
-import elementOver_2 from '@/public/img_decks/fake_decks/aqua-over.png';
-import elementOver_3 from '@/public/img_decks/fake_decks/black-over.png';
-import elementOver_4 from '@/public/img_decks/fake_decks/black-over.png';
-import elementOver_5 from '@/public/img_decks/fake_decks/black-over.png';
-import elementOver_6 from '@/public/img_decks/fake_decks/black-over.png';
+import elementOver_2 from '@/public/img_decks/fake_decks/greyblue-over.png';
 import elementOver_7 from '@/public/img_decks/fake_decks/aqua-over.png';
-import elementOver_8 from '@/public/img_decks/fake_decks/black-over.png';
 import elementOver_9 from '@/public/img_decks/fake_decks/bluemarine-over.png';
 import elementProfile_1 from '@/public/img_decks/fake_decks/bluemarine-profile.png';
-import elementProfile_2 from '@/public/img_decks/fake_decks/aqua-profile.png';
-import elementProfile_3 from '@/public/img_decks/fake_decks/black-profile.png';
-import elementProfile_4 from '@/public/img_decks/fake_decks/black-profile.png';
-import elementProfile_5 from '@/public/img_decks/fake_decks/black-profile.png';
-import elementProfile_6 from '@/public/img_decks/fake_decks/black-profile.png';
+import elementProfile_2 from '@/public/img_decks/fake_decks/greyblue-profile.png';
 import elementProfile_7 from '@/public/img_decks/fake_decks/aqua-profile.png';
-import elementProfile_8 from '@/public/img_decks/fake_decks/black-profile.png';
 import elementProfile_9 from '@/public/img_decks/fake_decks/bluemarine-profile.png';
-
 import girlUnder_1 from '@/public/img_decks/girl/deck_girl1.jpg';
 import girlUnder_2 from '@/public/img_decks/girl/deck_girl2.jpg';
 import girlUnder_3 from '@/public/img_decks/girl/deck_girl3.jpg';
@@ -90,22 +64,18 @@ import girlUnder_7 from '@/public/img_decks/girl/deck_girl7.jpg';
 import girlUnder_8 from '@/public/img_decks/girl/deck_girl8.jpg';
 import girlUnder_9 from '@/public/img_decks/girl/deck_girl9.jpg';
 import girlOver_1 from '@/public/img_decks/fake_decks/bluemarine-over.png';
-import girlOver_2 from '@/public/img_decks/fake_decks/black-over.png';
 import girlOver_3 from '@/public/img_decks/fake_decks/greyblue-over.png';
 import girlOver_4 from '@/public/img_decks/fake_decks/yellow-over.png';
 import girlOver_5 from '@/public/img_decks/fake_decks/yellow-over.png';
 import girlOver_6 from '@/public/img_decks/fake_decks/red-over.png';
 import girlOver_7 from '@/public/img_decks/fake_decks/red-over.png';
-import girlOver_8 from '@/public/img_decks/fake_decks/black-over.png';
 import girlOver_9 from '@/public/img_decks/fake_decks/red-over.png';
 import girlProfile_1 from '@/public/img_decks/fake_decks/bluemarine-profile.png';
-import girlProfile_2 from '@/public/img_decks/fake_decks/black-profile.png';
 import girlProfile_3 from '@/public/img_decks/fake_decks/greyblue-profile.png';
 import girlProfile_4 from '@/public/img_decks/fake_decks/yellow-profile.png';
 import girlProfile_5 from '@/public/img_decks/fake_decks/yellow-profile.png';
 import girlProfile_6 from '@/public/img_decks/fake_decks/red-profile.png';
 import girlProfile_7 from '@/public/img_decks/fake_decks/red-profile.png';
-import girlProfile_8 from '@/public/img_decks/fake_decks/black-profile.png';
 import girlProfile_9 from '@/public/img_decks/fake_decks/red-profile.png';
 
 export const imgSetBakerDeck: ImgSetProps[] = [
@@ -123,27 +93,27 @@ export const imgSetBakerDeck: ImgSetProps[] = [
     },
     {
         id: 4,
-        images: [bakerOver_4, bakerUnder_4, bakerProfile_4]
+        images: [blackOver, bakerUnder_4, blackProfile]
     },
     {
         id: 5,
-        images: [bakerOver_5, bakerUnder_5, bakerProfile_5]
+        images: [bakerOver, bakerUnder_5, bakerProfile]
     },
     {
         id: 6,
-        images: [bakerOver_6, bakerUnder_6, bakerProfile_6]
+        images: [blackOver, bakerUnder_6, blackProfile]
     },
     {
         id: 7,
-        images: [bakerOver_7, bakerUnder_7, bakerProfile_7]
+        images: [blindOver_5, bakerUnder_7, blindProfile_5]
     },
     {
         id: 8,
-        images: [bakerOver_8, bakerUnder_8, bakerProfile_8]
+        images: [blackOver, bakerUnder_8, blackProfile]
     },
     {
         id: 9,
-        images: [bakerOver_8, bakerUnder_8, bakerProfile_8]
+        images: [blackOver, bakerUnder_9, blackProfile]
     }
 ];
 
@@ -158,7 +128,7 @@ export const imgSetBlindDeck: ImgSetProps[] = [
     },
     {
         id: 12,
-        images: [blindOver_3, blindUnder_3, blindProfile_3]
+        images: [blackOver, blindUnder_3, blackProfile]
     },
     {
         id: 13,
@@ -174,7 +144,7 @@ export const imgSetBlindDeck: ImgSetProps[] = [
     },
     {
         id: 16,
-        images: [blindOver_7, blindUnder_7, blindProfile_7]
+        images: [blackOver, blindUnder_7, blackProfile]
     },
     {
         id: 17,
@@ -182,7 +152,7 @@ export const imgSetBlindDeck: ImgSetProps[] = [
     },
     {
         id: 18,
-        images: [blindOver_9, blindUnder_9, blindProfile_9]
+        images: [blackOver, blindUnder_9, blackProfile]
     }
 ];
 
@@ -197,19 +167,19 @@ export const imgSetElementDeck: ImgSetProps[] = [
     },
     {
         id: 21,
-        images: [elementOver_3, elementUnder_3, elementProfile_3]
+        images: [blackOver, elementUnder_3, blackProfile]
     },
     {
         id: 22,
-        images: [elementOver_4, elementUnder_4, elementProfile_4]
+        images: [blackOver, elementUnder_4, blackProfile]
     },
     {
         id: 23,
-        images: [elementOver_5, elementUnder_5, elementProfile_5]
+        images: [blackOver, elementUnder_5, blackProfile]
     },
     {
         id: 24,
-        images: [elementOver_6, elementUnder_6, elementProfile_6]
+        images: [blackOver, elementUnder_6, blackProfile]
     },
     {
         id: 25,
@@ -217,7 +187,7 @@ export const imgSetElementDeck: ImgSetProps[] = [
     },
     {
         id: 26,
-        images: [elementOver_8, elementUnder_8, elementProfile_8]
+        images: [blackOver, elementUnder_8, blackProfile]
     },
     {
         id: 27,
@@ -232,7 +202,7 @@ export const imgSetGirlDeck: ImgSetProps[] = [
     },
     {
         id: 29,
-        images: [girlOver_2, girlUnder_2, girlProfile_2]
+        images: [blackOver, girlUnder_2, blackProfile]
     },
     {
         id: 30,
@@ -256,7 +226,7 @@ export const imgSetGirlDeck: ImgSetProps[] = [
     },
     {
         id: 35,
-        images: [girlOver_8, girlUnder_8, girlProfile_8]
+        images: [blackOver, girlUnder_8, blackProfile]
     },
     {
         id: 36,
